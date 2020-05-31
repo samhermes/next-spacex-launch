1b33af9f471da4f55903f17b0b1addf88bd6efc6
Sam Hermes <samhermes@me.com>
2020-05-30 20:37:21 -0500

    Updates

---
+++ precache-manifest.a50dc1bf92feb2a9cdbc0435e8bda0bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418596c4f1dc05fe36b59eb30b5c3308",
+    "url": "/next-spacex-launch/index.html"
+  },
+  {
+    "revision": "149ce05f01d686327509",
+    "url": "/next-spacex-launch/static/css/main.087ec26e.chunk.css"
+  },
+  {
+    "revision": "8e377432d0c8dd994dbd",
+    "url": "/next-spacex-launch/static/js/2.8f6b5c85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/next-spacex-launch/static/js/2.8f6b5c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149ce05f01d686327509",
+    "url": "/next-spacex-launch/static/js/main.beb3de66.chunk.js"
+  },
+  {
+    "revision": "79eb5e71d5bcb7e9ba66",
+    "url": "/next-spacex-launch/static/js/runtime-main.1495435f.js"
+  },
+  {
+    "revision": "26f1aa60aea23beee6c1092e4d42b6fe",
+    "url": "/next-spacex-launch/static/media/spacex-bg-large.26f1aa60.jpg"
+  },
+  {
+    "revision": "4b63988c0df711cc64843c295752bfc7",
+    "url": "/next-spacex-launch/static/media/spacex-bg-small.4b63988c.jpg"
+  }
+]);
